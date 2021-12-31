e34eaaa6067e4559688695c67a0623dee8bb12b5
TEnLOcODE <87935299+TEnLOcODE@users.noreply.github.com>
2021-12-31 15:04:25 +0200

    i added hebrew support... can u apply it ??

---
+++ locales/js-year-calendar.he.js
@@ -0,0 +1,17 @@
+/**
+ * Catalan translation for js-year-calendar
+ * David Ramirez <david@davidrv.com>
+ * Based on 
+ * Catalan translation for bootstrap-datepicker
+ * J. Garcia <jogaco.en@gmail.com>
+ */
+ 
+Calendar.locales['ca'] = {
+	days: ["ראשון", "שני", "שלישי", "רביעי", "חמישי", "שישי", "שבת"],
+	daysShort: ["א", "ב", "ג", "ד", "ה", "ו", "ש"],
+	daysMin: ["רא", "שנ", "של", "רב", "חמ", "שי", "שב"],
+	months: ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני", "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"],
+	monthsShort: ["ינו", "פבר", "מרץ", "אפר", "מאי", "יוני", "יולי", "אוג", "ספט", "אוק", "נוב", "דצמ"],
+	weekShort: 'S',
+	weekStart: 1
+};
